24aadb0eadf460012ae9ebe00466101ba05e9401
Tony <anthonyhealy90@gmail.com>
2024-10-31 12:56:54 -0300

    added needed things

--- app.js
+++ app.js
@@ -2,18 +2,43 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+
+const server = http.createServer(app);
+const io = new Server(server);
+
 const PORT = 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
-// Route to serve the sound file
+// Serve static files from the "sounds" directory
+app.use('/sounds', express.static(path.join(__dirname, 'sounds')));
+
+// Route to serve the "ScaryLaugh" sound file
 app.get('/sound/jackolantern', (req, res) => {
     const soundPath = path.join(__dirname, 'sounds', 'ScaryLaugh.mp3');
     res.sendFile(soundPath);
 });
 
+// Route to serve the "BackgroundEerieMusic" sound file
+app.get('/sound/BackgroundEerieMusic.mp3', (req, res) => {
+    const soundPath = path.join(__dirname, 'sounds', 'BackgroundEerieMusic.mp3');
+    res.sendFile(soundPath);
+});
+
+// Socket.io for real-time interactions
+io.on('connection', (socket) => {
+    console.log('A visitor has connected!');
+    
+    // Example: Emit a spooky event to the client every 10 seconds
+    setInterval(() => {
+        socket.emit('spookyEvent', { type: 'random' });
+    }, 10000);
+});
+
 // Start the server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Spooky Sound Show running at http://localhost:${PORT}`);
 });
